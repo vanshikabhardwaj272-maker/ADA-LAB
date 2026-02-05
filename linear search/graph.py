55a21bd76212fd0a27c4c4c492bda549867eee57
vanshikabhardwaj272-maker <vanshikabhardwaj272@gmail.com>
2026-02-05 23:52:32 +0530

    Create graph.py

---
+++ linear search/graph.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+# Input size
+n = [10, 50, 100, 200, 500, 1000]
+
+# Time taken (linear growth - O(n))
+time = [1, 5, 10, 20, 50, 100]
+
+plt.figure()
+plt.plot(n, time)
+plt.xlabel("Number of Elements (n)")
+plt.ylabel("Time Taken")
+plt.title("Linear Search Time Complexity Graph (O(n))")
+plt.show()
